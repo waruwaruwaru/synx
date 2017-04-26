3b8257ec6dc190208dafcd760adeec3082cb04da
Walt Tran <wtran@ucdavis.edu>
2017-04-26 13:41:36 -0700

    got google map to add markers, need to implement on geolocation api call now...

--- client/src/components/feature.js
+++ client/src/components/feature.js
@@ -4,39 +4,43 @@ import * as actions from '../actions';
 import GoogleMap from './google_maps';
 import { reduxForm } from 'redux-form';
 
+
 class Feature extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      map: null,
       markers: []
     }
   }
   componentWillMount() {
     this.props.fetchMessage();
+    //this.props.getMarkers();
   }
 
   onSubmit(props) {
-    console.log("hello");
+    /*Implement an add Marker function
+    Do an HTTP call to some API that can get the location
+    Save the location data into MongoDB
+    redirect user to /feature, it should refresh and display all Markers in the database
+    */
   }
 
   render() {
     const { fields: { game, address }, handleSubmit } = this.props;
-    console.log(this.props);
     return (
       <div id="feature">
         {this.props.message}
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <fieldset className="form-group">
             <label>Game:</label>
-            <input className="form-control" />
+            <input type="text" className="form-control" {...game} />
           </fieldset>
 
           <fieldset className="form-group">
             <label>Address:</label>
-            <input className="form-control" />
+            <input type="text" className="form-control" {...address} />
           </fieldset>
-          <button action="submit" className="btn btn-primary">Submit!</button>
+          <button type="submit" className="btn btn-primary">Submit!</button>
         </form>
         <GoogleMap />
       </div>
--- client/src/components/google_maps.js
+++ client/src/components/google_maps.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      map: null,
+      markers: []
+    }
+  }
+
   componentDidMount() {
     var uluru = {lat: 37.8044, lng: -122.419416};
-    var map = new google.maps.Map(this.refs.map, {
-      zoom: 12,
-      center: uluru
+    this.state.map = new google.maps.Map(this.refs.map, {
+    zoom: 12,
+    center: uluru
     });
 
     var marker = new google.maps.Marker({
       position: uluru,
-      map: map
+      map: this.state.map
     });
+  }
+
+  addMarker() {
+    this.setState({markers: [{lat: 38.8044, lng: -122.419416}, {lat: 40.8044, lng: -122.419416}, {lat: 50.8044, lng: -122.419416}]});
+    var marks = [];
+    for(var i = 0; i < this.state.markers.length; i++) {
+      console.log(this.state.markers[i]);
+      marks[i] = new google.maps.Marker({
+        position: this.state.markers[i],
+        map: this.state.map
+      });
+    }
   }
 
   render() {
     return(
-      <div ref="map" id="googleMap" />
+      <div>
+        <div ref="map" id="googleMap" />
+        <button onClick={this.addMarker.bind(this)}>Button</button>
+      </div>
     );
   }
 }
